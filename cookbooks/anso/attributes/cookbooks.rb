b4beb682ef0d62f8e68f134460c8d68c69d6a60e
Brian Waldon <bcwaldon@gmail.com>
2012-01-31 00:14:26 -0800

    Removing anso cookbook

--- cookbooks/anso/attributes/cookbooks.rb
+++
@@ -1,5 +0,0 @@
-
-default[:cookbooks][:url] = "git://github.com/ansolabs/openstack-cookbooks.git"
-default[:cookbooks][:branch] = "master"
-default[:cookbooks][:user] = "vagrant"
-default[:cookbooks][:copy_dir] = "/vagrant"
--- cookbooks/anso/attributes/settings.rb
+++
@@ -1,4 +0,0 @@
-
-default[:settings][:user] = "vagrant"
-default[:settings][:group] = "vagrant"
-default[:settings][:repository] = "http://github.com/vishvananda/settings.git -b linux"
